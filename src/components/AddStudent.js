168c755cdbd40029b34aeed742b06a18bf6f066a
raedawwad95 <awwad12121@gmail.com>
2018-10-27 20:37:36 +0200

    edit Student

--- src/components/AddStudent.js
+++ src/components/AddStudent.js
@@ -10,7 +10,8 @@ class AddStudent extends React.Component {
     this.state={
     students:[],
     fullName:'',
-    level:''
+    level:'',
+    id:''
   }
   }
   onChange(e){
@@ -52,6 +53,15 @@ $.ajax({
 }
   edit(id){
     console.log(id)
+  var that =this;
+  $.ajax({
+  type:'put',
+    url: '/students/edit',
+    data:{id:that.state.id,level:that.state.level},
+    success:function(data){
+      alert('Edit Student')
+    }
+  });   
   }
   delete(id){
   $.ajax({
@@ -61,6 +71,12 @@ $.ajax({
       alert('Delete Student')
     }
   });   
+  }
+
+  editId(id){
+    this.setState({
+      id:id
+    })
   }
   
   render() {
@@ -89,7 +105,8 @@ $.ajax({
                 <tr key={{index}}>
                   <td>{item.fullName}</td>
                   <td>{item.level}</td>
-                  <td><input type="submit" value="Edit" onClick={this.edit.bind(this,item._id)} style={{'borderRadius': '25px', 'backgroundColor':'#D17E7E'}}/></td>
+                  <td><input type="submit" value="Edit" data-toggle="modal" data-target="#myModal" onClick={this.editId.bind(this,item._id)} style={{'borderRadius': '25px', 'backgroundColor':'#D17E7E'}}/></td>
+                  
                   <td><input type="submit" value="Delete" onClick={this.delete.bind(this,item._id)} style={{'borderRadius': '25px', 'backgroundColor':'#D17E7E'}}/></td>
                 </tr>
                 )
@@ -98,6 +115,24 @@ $.ajax({
             </table>
         </div>
       </div>
+      <div className="modal fade" id="myModal">
+    <div className="modal-dialog" >
+    
+      <div className="modal-content" style={{'backgroundImage' :'linear-gradient(#FEE8E8, #FADBDB, #F6E3E3)'}}>
+        <div className="modal-header">
+          <button type="button" className="close" data-dismiss="modal">&times;</button>
+        </div>
+        <div className="modal-body">
+          <input type="text" placeholder="Level(1-5)" name="level" onChange={this.onChange}/>
+        </div>
+        <div className="modal-footer">
+          <button type="button" className="btn btn-default" onClick={this.edit.bind(this)} style={{'borderRadius': '25px', 'backgroundColor':'#D17E7E'}}>Edit Data</button>
+          <button type="button" className="btn btn-default" data-dismiss="modal" style={{'borderRadius': '25px', 'backgroundColor':'#D17E7E'}}>Close</button>
+        </div>
+      </div>
+      
+    </div>
+  </div>
       </div>
     )  
   }
