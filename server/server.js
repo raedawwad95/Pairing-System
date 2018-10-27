3a21e88a75217f55800000ebc4599b593e5b1c00
raedawwad95 <awwad12121@gmail.com>
2018-10-27 19:00:47 +0200

    router for pairing

--- server/server.js
+++ server/server.js
@@ -2,9 +2,11 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var db = require('./db');
 var Students = require('./resources/Students/StudentsRouter');
+var Pairing = require('./resources/Pairing/PairingRouter');
 var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use('/students',Students)
+app.use('/students',Students);
+app.use('/pairing',Pairing);
 
 module.exports = app;
